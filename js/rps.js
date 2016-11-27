29c51ef137d2c7e00cf05b3d3505ff3b4a388d5c
i3wgnit <i3wgnit@gmail.com>
2016-11-26 19:02:31 -0500

    added boxes for future reference

--- js/rps.js
+++ js/rps.js
@@ -10,9 +10,14 @@ CANVAS.update = function() {
 CANVAS.refresh = function() {
     CANVAS.Draw.clear();
 
+    CANVAS.Draw.rect( 1280 - 240, 0, 240, 240 ).stroke();
+    CANVAS.Draw.rect( 1280 - 240, 240, 240, 240 ).stroke();
+    CANVAS.Draw.rect( 1280 - 240, 480, 240, 240 ).stroke();
+
     var txt = "MousePos: " + GAME.touch.x + ", " + GAME.touch.y;
-    CANVAS.Draw.text( txt, 1, 1, 18, "black" );
-    CANVAS.Draw.text( "" + !!GAME.touch.press, 1, 20, 18, "black" );
+    CANVAS.Draw.text( txt, 1, 2, 16, "black" );
+    CANVAS.Draw.text( GAME.touch.id, 1, 16, 16, "black" );
+    CANVAS.Draw.text( "" + !!GAME.touch.press, 1, 32, 16, "black" );
 };
 
 var GAME = {};
@@ -30,17 +35,35 @@ if ( CANVAS.MOBILE ) {
 
         var touch = event.touches[0],
             rect = CANVAS.doc.getBoundingClientRect();
+
         GAME.touch.x = touch.clientX - rect.left;
+        GAME.touch.x *= CANVAS.WIDTH / CANVAS.currentWidth;
+
         GAME.touch.y = touch.clientY - rect.top;
-        GAME.touch.press = true;
+        GAME.touch.y *= CANVAS.HEIGHT / CANVAS.currentHeight;
+
         GAME.touch.id = touch.identifier;
+
+        GAME.touch.press = true;
+    }, false );
+
+    window.addEventListener( "touchmove", function( event ) {
+        event.preventDefault();
+        var touch = event.touches[0],
+            rect = CANVAS.doc.getBoundingClientRect();
+
+        GAME.touch.x = touch.clientX - rect.left;
+        GAME.touch.x *= CANVAS.WIDTH / CANVAS.currentWidth;
+
+        GAME.touch.y = touch.clientY - rect.top;
+        GAME.touch.y *= CANVAS.HEIGHT / CANVAS.currentHeight;
     }, false );
 
     window.addEventListener( "touchend", function( event ) {
         event.preventDefault();
 
         var touches = event.changedTouches;
-        for ( var i = 0; i < touches.length; i++ ) {
+        for ( var i = 0; i < touches.length; i += 1 ) {
             if ( GAME.touch.id == touches[i].identifier ) {
                 GAME.touch.press = false;
             }
